539a6b777584c2d9a50371d6a816e16c7b381289
JustinPrins98 <justinprinsroc1998@gmail.com>
2024-03-07 10:58:33 +0100

    Alle bouwstenen (basics) af in HTML

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("Kaaspizza HE!")
+
+// uitleg script: 
+document.getElementById('datePickerId').max = new Date().toLocaleDateString('fr-ca')
+document.getElementById('datePickerId').min = new Date(new Date().setMonth(new Date().getMonth() - 8)).toLocaleDateString('fr-ca');
+
+
