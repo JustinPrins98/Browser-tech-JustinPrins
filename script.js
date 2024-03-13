34eb2e44e1b4779ae4934216540cfd3b5f155ebf
JustinPrins98 <justinprinsroc1998@gmail.com>
2024-03-13 11:35:46 +0100

    JS HTML uitklap functie

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ document.getElementById('datePickerId').max = new Date().toLocaleDateString('fr-
 document.getElementById('datePickerId').min = new Date(new Date().setMonth(new Date().getMonth() - 8)).toLocaleDateString('fr-ca');
 
 
+
+
+
+// Uitklappen
+
+document.addEventListener('DOMContentLoaded', function () {
+    hideFieldset();
+});
+
+function showFieldset() {
+    var fieldset = document.getElementById('additionalInfoFieldset');
+    fieldset.style.display = 'block';
+}
+
+function hideFieldset() {
+    var fieldset = document.getElementById('additionalInfoFieldset');
+    fieldset.style.display = 'none';
+}
+
+
